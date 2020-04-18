772949690a71a5702e36d0b6010fad9878d5282a
Sujeet Thakur <iamsujeet1308@gmail.com>
2020-04-18 21:32:02 +0530

    Set Up the basic layout of the app

---
+++ recipeBookApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.modal'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('A Test Recipe','Simply a Test Recipe',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
+    new Recipe('A Test Recipe2','Simply a Second Test Recipe',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

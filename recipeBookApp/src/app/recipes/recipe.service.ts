b65e423a397baedd621de71c720f272774945aa7
Sujeet Thakur <iamsujeet1308@gmail.com>
2020-04-26 11:12:49 +0530

    replaced EventEmitter with Subjects

--- recipeBookApp/src/app/recipes/recipe.service.ts
+++ recipeBookApp/src/app/recipes/recipe.service.ts
@@ -1,39 +1,41 @@
 import { Recipe } from './recipe.modal';
-import { EventEmitter, Injectable } from '@angular/core';
+import {  Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.modal';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
-export class RecipeService{
-    recipeSelected=new EventEmitter<Recipe>();
-    private recipes: Recipe[]=[
-        new Recipe('Khichdi','Tasty simple and healthy recipe',
+export class RecipeService {
+    recipeSelected = new Subject<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe('Khichdi', 'Tasty simple and healthy recipe',
         'https://www.cookwithmanali.com/wp-content/uploads/2019/02/Instant-Pot-Quinoa-Khichdi.jpg',
         [
-            new Ingredient('rice',1),
-            new Ingredient('pulses',1)
+            new Ingredient('rice', 1),
+            new Ingredient('pulses', 1)
 
         ]),
-        new Recipe('Chicken Curry','A super tasty chicken curry recipe',
+        new Recipe('Chicken Curry', 'A super tasty chicken curry recipe',
         'https://www.theflavorbender.com/wp-content/uploads/2018/02/Sri-Lankan-Chicken-Curry-The-Flavor-Bender-Featured-Image-SQ-2.jpg',
         [
-            new Ingredient('chicken',1),
-            new Ingredient('onions',10)
+            new Ingredient('chicken', 1),
+            new Ingredient('onions', 10)
         ])
     ];
 
-    constructor(private slService:ShoppingListService){}
+    constructor(private slService: ShoppingListService) { }
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice();
     }
 
-    getRecipe(index:number){
+    getRecipe(index: number) {
         return this.recipes[index];
     }
 
-    addIngredientToShoppingList(ingredients:Ingredient[]){
+    addIngredientToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
 
     }
 }
+
--- recipeBookApp/src/app/shopping-list/shopping-list.service.ts
+++ recipeBookApp/src/app/shopping-list/shopping-list.service.ts
@@ -1,25 +1,27 @@
 import { Ingredient } from '../shared/ingredient.modal';
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
-export class ShoppingListService{
-    ingredientsChanged=new EventEmitter<Ingredient[]>();
 
-    private ingredients: Ingredient[] =[
-        new Ingredient('Apples',5),
-        new Ingredient('Tomatoes',10)
+export class ShoppingListService {
+    ingredientsChanged = new Subject<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
     ];
 
-    getIngredients(){
+    getIngredients() {
         return this.ingredients.slice();
     }
 
-    addIngredient(ingredient:Ingredient){
+    addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
-    addIngredients(ingredients:Ingredient[]){
+    addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }

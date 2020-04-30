f0db9243f4a064bfd1f32a9463c75f75aa6a4b8a
Sujeet Thakur <iamsujeet1308@gmail.com>
2020-04-30 19:34:08 +0530

    added FormsModule in app.module.ts and used NgForm to work with shopping list form

--- recipeBookApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipeBookApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,22 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.modal';
 import { ShoppingListService } from '../shopping-list.service';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput',{static:true}) nameInputRef:ElementRef;
-  @ViewChild('amountInput',{static:true}) amountInputRef:ElementRef;
-  
 
-  constructor(private slService:ShoppingListService) { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAddItem(){
-    const ingName=this.nameInputRef.nativeElement.value;
-    const ingAmount=this.amountInputRef.nativeElement.value;
-    const newIngredient=new Ingredient(ingName,ingAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
-    
-
   }
 
 }
